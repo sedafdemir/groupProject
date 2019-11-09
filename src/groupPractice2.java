eb93a4eb0cc820eed89c07919a4e3086c82f9a93
sedafdemir <57167050+sedafdemir@users.noreply.github.com>
2019-11-09 14:50:54 -0600

    bruch work

--- src/groupPractice2.java
+++ src/groupPractice2.java
@@ -1,6 +1,9 @@
 public class groupPractice2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello world");
+        System.out.println("Second Line");
+
+
     }
 }
